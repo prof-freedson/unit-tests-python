e8f2b34dbd6d87e3dacefd59355ac741cc6ff3c9
prof-freedson <freedson.prof@gmail.com>
2024-11-13 17:18:17 -0300

    Arquivo calculadoraIMC.py criado

---
+++ calculadoraIMC.py
@@ -0,0 +1,12 @@
+class CalculadoraIMC:
+    
+    def resultado(peso, altura):
+        imc = peso / (altura * altura)
+        if imc < 19:
+            return "magreza"
+        elif imc >= 19 and imc < 24:
+            return "normal"
+        elif imc >= 24 and imc < 29:
+            return "sobrepeso"
+        else:
+            return "obesidade"
